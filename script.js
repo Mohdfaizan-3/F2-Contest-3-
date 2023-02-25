7f59f7d084f8712159befa951bd1cfb629d71144
Mohd Faizan <faizan.mohd09@gmail.com>
2023-02-25 20:41:04 +0530

    completed

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const table = document.querySelector("table");
 const addNewRow = document.querySelector(".add-row-btn");
-
+const tableData = [];
 function Constructor(
-  name,
-  rollNo,
+  id,
+  student_name,
+  student_roll,
   subject,
   marks,
   markedBy
 ) {
-  this.name = name;
-  this.rollNo = rollNo;
+  this.id = id;
+  this.student_name = student_name;
+  this.student_roll = student_roll;
   this.subject = subject;
   this.marks = marks;
   this.markedBy = markedBy;
@@ -21,15 +23,42 @@ function handleValue(buttonElement) {
   const parentElement =
     buttonElement.parentElement.parentElement;
   const cells = parentElement.cells;
+  const data = {};
+
   for (let i = 1; i < cells.length - 1; i++) {
-    const value = cells[i].querySelector("input").value;
-    console.log(value);
+    const input = cells[i].querySelector("input");
+    data[input.name] = input.value;
   }
+
+  const rowId = cells[0].textContent;
+  console.log(rowId);
+  const objIndex = rowId - 1;
+  if (tableData[objIndex] !== undefined) {
+    const obj = tableData[objIndex];
+    obj.student_name = data.student_name;
+    obj.student_roll = data.student_roll;
+    obj.subject = data.subject;
+    obj.marks = data.marks;
+    obj.markedBy = data.markedBy;
+  } else {
+    const obj = new Constructor(
+      cells[0].textContent,
+      data.student_name,
+      data.student_roll,
+      data.subject,
+      data.marks,
+      data.markedBy
+    );
+
+    tableData.push(obj);
+  }
+
+  console.log(tableData);
 }
 
 function handleEdit(event) {
   const buttonElement = event.target;
-  
+
   const parentElement =
     buttonElement.parentElement.parentElement;
 
@@ -39,7 +68,6 @@ function handleEdit(event) {
     inputs.forEach((input) => {
       input.disabled = false;
     });
-    
   } else {
     buttonElement.textContent = "Edit";
     const inputs = parentElement.querySelectorAll("input");
@@ -50,44 +78,48 @@ function handleEdit(event) {
   }
 }
 
-
-
-
 function createData() {
   const newRow = document.createElement("tr");
 
   const newRowId = table.rows.length;
   const id = document.createElement("td");
   id.textContent = newRowId;
+  id.name = "id";
   id.setAttribute("style", "text-align: center;");
 
   const Namecell = document.createElement("td");
-  const input = document.createElement("input");
-  input.type = "text";
-  input.disabled = true;
-  Namecell.appendChild(input);
+  const inputName = document.createElement("input");
+  
+  inputName.name = "student_name";
+  inputName.type = "text";
+  inputName.disabled = true;
+  Namecell.appendChild(inputName);
 
   const rollNocell = document.createElement("td");
-  const inputrollNocell = document.createElement("input");
-  inputrollNocell.type = "number";
-  inputrollNocell.disabled = true;
-  rollNocell.appendChild(inputrollNocell);
+  const inputRollNo = document.createElement("input");
+  inputRollNo.type = "number";
+  inputRollNo.name = "student_roll";
+  inputRollNo.disabled = true;
+  rollNocell.appendChild(inputRollNo);
 
   const subjectcell = document.createElement("td");
   const inputsubject = document.createElement("input");
   inputsubject.type = "text";
+  inputsubject.name = "subject";
   inputsubject.disabled = true;
   subjectcell.appendChild(inputsubject);
 
   const markscell = document.createElement("td");
   const inputmarks = document.createElement("input");
   inputmarks.type = "number";
+  inputmarks.name = "marks";
   inputmarks.disabled = true;
   markscell.appendChild(inputmarks);
 
   const markedBycell = document.createElement("td");
   const inputmarkedBy = document.createElement("input");
   inputmarkedBy.type = "email";
+  inputmarkedBy.name = "markedBy";
   inputmarkedBy.disabled = true;
   markedBycell.appendChild(inputmarkedBy);
 
